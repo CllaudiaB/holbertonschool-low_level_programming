c3f9d345204ea55adc64ac9fc870799564211809
CllaudiaB <buraanna27@gmail.com>
2022-09-30 06:18:24 -0700

    task 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ *main printf function
+ *Description: prin function
+ *Return: return 0 to exit
+ */
+
+int main(void)
+{
+printf("with proper grammer, but the outcome is a piece of art,")
+
+return (0);
+}
