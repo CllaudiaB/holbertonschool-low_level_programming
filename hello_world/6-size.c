cca2982e7751f5c602837a5d97f67cff79e15c03
CllaudiaB <buraanna27@gmail.com>
2022-09-30 08:23:11 -0700

    task 6

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,24 +1,16 @@
-#include<stdio.h>
+#include <stdio.h>
 
-/**
- *main - size of data type
- *Description: reutrn size
- *Return: 0 if all is good
+/*
+ * main - Main function
+ *
+ * Return: Exit status code
  */
-
 int main(void)
 {
-int int_type;
-char char_type;
-long int long_int_type;
-long long int long_long_int_type;
-float float_type;
-
-printf("Size of int: %zu byte\(s\)\n", sizeof(int_type));
-printf("Size of char: %zu byte\(s\)\n", sizeof(char_type));
-printf("Size of long int: %zu byte\(s\)\n", sizeof(long_int_type));
-printf("Size of long long int: %zu\(s\)\n", sizeof(long_long_int));
-printf("Size of float: %zu\(s\)\n", sizeof(float_type));
-
-return (0);
+	printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d bytes(s)\n", sizeof(int));
+	printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
+	printf("Size of a float: %d bytes(s)\n", sizeof(float));
+	return (0);
 }
