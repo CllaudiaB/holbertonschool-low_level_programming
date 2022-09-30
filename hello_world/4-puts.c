441fc5d093950fe4742781643cdde1a5b279bf87
CllaudiaB <buraanna27@gmail.com>
2022-09-30 05:53:01 -0700

    task 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * Display the message to the screen
@@ -7,8 +7,8 @@
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 
 
-return 0;
+return(0);
 }
