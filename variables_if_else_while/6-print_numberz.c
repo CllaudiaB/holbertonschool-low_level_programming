57972bcad5c5de22f2a300dbec0935f7a6b7ed44
CllaudiaB <buraanna27@gmail.com>
2022-10-03 10:47:31 -0700

    task 6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *main - Print number
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	n = '0';
+
+	while (n <= '9')
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
