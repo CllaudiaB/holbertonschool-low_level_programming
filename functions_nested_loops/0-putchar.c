f311aabce16f96f5eb81d3787dc537bdff083ae5
CllaudiaB <buraanna27@gmail.com>
2022-10-04 08:29:11 -0700

    task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,13 @@
 #include "main.h"
 
 /**
- *main - Print _putchar
+ *main - Prints _putchar
  *
- *Return: Always 0 (Success)
+ *Return: Always 0
  */
 
 int main(void)
 {
-
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
