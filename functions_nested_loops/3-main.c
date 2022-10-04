e406c9bdb4e2309a9c294bd4944014ccc69b34a2
CllaudiaB <buraanna27@gmail.com>
2022-10-04 11:42:41 -0700

    task 3

---
+++ functions_nested_loops/3-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *main - checks lowercase character
+ *
+ *Return: 1 if lowercase, 0 otherwise
+ */
+
+int main(void)
+{
+	int r;
+
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
