fdde72f19f3f36f22609b73072b71dc9c9fcb7e0
CllaudiaB <buraanna27@gmail.com>
2022-10-04 09:07:47 -0700

    task 1

---
+++ functions_nested_loops/1-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *function that prints the alphabet
+ *
+ *Return: void
+ */
+
+void print_alphabet(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		_putchar(alph);
+	}
+	_putchar('\n');
+}
