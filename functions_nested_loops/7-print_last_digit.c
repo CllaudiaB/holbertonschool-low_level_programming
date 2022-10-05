b25c4147a2f768422630495752d0626999648ae4
CllaudiaB <buraanna27@gmail.com>
2022-10-05 02:47:55 -0700

    task 7

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,20 +10,21 @@
 int print_last_digit(int n)
 {
 	int digit;
+
 	if (n > 0)
 	{
 		digit = n % 10;
-		_putchar(digit);
+		_putchar(digit + '0');
 	}
 	else if (n == 0)
 	{
 		digit = 0;
-		_putchar(digit);
+		_putchar(digit + '0');
 	}
 	else
 	{
 		digit = n % 10 * (-1);
-		_putchar(digit);
+		_putchar(digit + '0');
 	}
 	return (digit);
 }
