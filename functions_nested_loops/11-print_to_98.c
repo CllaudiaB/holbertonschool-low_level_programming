70093c95bcd03da3605764833ef8774e697d9c96
CllaudiaB <buraanna27@gmail.com>
2022-10-06 02:01:41 -0700

    task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -10,16 +10,13 @@
 
 void print_to_98(int n)
 {
-	if (n = 98)
-	{
 		while (n < 98)
 		{
 			printf("%d, ", n);
 			n++;
 		}
 		printf("\n");
-	}
-	else
+
 		while (n > 98)
 		{
 			printf("%d, ", n);
