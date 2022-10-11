81107b246aa852237b2c4c24668e3432a91d7cbd
CllaudiaB <buraanna27@gmail.com>
2022-10-11 02:16:59 -0700

    task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -17,10 +17,10 @@ void rev_string(char *s)
 		len++;
 	}
 
-	for (i = len - 1; i >= (len - 1) /2; i--)
+	for (i = len - 1; i >= (len - 1) / 2; i--)
 	{
 		temp = s[i];
-		s[i] = s[len - i -1];
+		s[i] = s[len - i - 1];
 		s[len - i - 1] = temp;
 	}
 }
