c2c1f5a4d0ef8e967c4a2d4a95239695c347bf11
CllaudiaB <buraanna27@gmail.com>
2022-10-12 03:54:50 -0700

    task 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,7 +9,7 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int len_s1 = 0, len_s2 = 0;
+	int len_s1 = 0, len_s2 = 0, n = 0;
 
 	while (s1[len_s1] != '\0')
 	{
@@ -19,11 +19,6 @@ int _strcmp(char *s1, char *s2)
 	{
 		len_s2++;
 	}
-
-	if (len_s1 < len_s2)
-		return (-1);
-	else if (len_s1 > len_s2)
-		return (1);
-	else
-		return (0);
+	n = len_s1 - len_s2;
+	return (n);
 }
