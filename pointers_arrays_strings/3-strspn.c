6815fe65b6fcb824de10e24d244a07333128d0be
CllaudiaB <buraanna27@gmail.com>
2022-10-14 07:11:37 -0700

    task 3

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -11,11 +11,11 @@ unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i = 0, n = 0, j = 0;
 
-	for (i = 0; s[i] != '\0';i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (s[i] != ' ')
 		{
-			for (j = 0; accept[j] != '\0';j++)
+			for (j = 0; accept[j] != '\0'; j++)
 			{
 				if (accept[j] == s[i])
 					n++;
