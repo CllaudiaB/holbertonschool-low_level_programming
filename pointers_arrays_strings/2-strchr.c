9bba6635251416ce937a716be8938673cd46ca57
CllaudiaB <buraanna27@gmail.com>
2022-10-14 02:21:21 -0700

    task 2

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_strchr - function that locates a character in a string
+ *@s: parameter of the function
+ *@c: parameter of the function
+ *Return: char
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		if (s[i] == c && s[i + 1] == s[i])
+			return (s);
+	}
+	return (0);
+}
