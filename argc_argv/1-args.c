48c57232e86a24b80c01077f29ed5718abb2b2f3
CllaudiaB <buraanna27@gmail.com>
2022-10-19 03:02:20 -0700

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main - Main function
+ *@argc: argument of main
+ *@argv: arfument of main
+ *
+ *Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+        printf("%d\n", argc - 1);
+
+	return (0);
+}
