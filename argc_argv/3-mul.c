21e89e5d54d5fea592769a251501fd66a52db3e2
CllaudiaB <buraanna27@gmail.com>
2022-10-19 05:32:10 -0700

    task 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -12,11 +12,12 @@
 int main(int argc, char *argv[])
 {
 	int i, mult = 1;
+
 	if (argc > 2)
 	{
 		for (i = 1; i < argc; i++)
 		{
-	       		mult *= atoi(argv[i]);
+			mult *= atoi(argv[i]);
 		}
 		printf("%d\n", mult);
 	}
