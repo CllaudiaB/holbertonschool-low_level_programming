6d8e23df738e53775c6ca50c3fdba79edccb2960
CllaudiaB <buraanna27@gmail.com>
2022-10-24 11:13:49 -0700

    task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,16 +17,17 @@ char *str_concat(char *s1, char *s2)
 	p = malloc(sizeof(s1) + sizeof(s2) + 1);
 	if (p == NULL)
 		return (NULL);
+	if (s1 == NULL || s2 == NULL)
+	{
+		s1 = ' ';
+		s2 = ' ';
+	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{
-		if (s1 == NULL)
-			s1 = " ";
 		p[i] = s1[i];
 	}
 	for (j = 0; s2[j] != '\0'; j++)
 	{
-		if (s2 == NULL)
-			s2 = " ";
 		p[i] = s2[j];
 		i++;
 	}
