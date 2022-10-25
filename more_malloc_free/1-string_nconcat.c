ddf628280656a29f3e8ede455664376b6c60d754
CllaudiaB <buraanna27@gmail.com>
2022-10-25 10:40:32 -0700

    task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 			return (NULL);
 		p[i] = s1[i];
 	}
-	for (j = 0; j <= n; j++)
+	for (j = 0; j < n; j++)
 	{
 		if (p == NULL)
 			return (NULL);
