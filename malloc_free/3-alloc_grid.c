42f943d6ced3b035cd495f5fd5ee43e2da2ce5f2
CllaudiaB <buraanna27@gmail.com>
2022-10-25 05:14:29 -0700

    task 3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -13,21 +13,29 @@
 int **alloc_grid(int width, int height)
 {
 	int **p;
-	int i;
+	int i, j;
 
 	if (width == 0 || height == 0)
 		return (NULL);
+
 	p = malloc(height * sizeof(int));
 	if (p == NULL)
 		return (NULL);
+
 	for (i = 0; i < height; i++)
 	{
-		if (p[i] == NULL)
-		{
-
-		}
-
 		p[i] = malloc(width * sizeof(int));
+		if (p == NULL)
+		{
+			for (j = 0; j < height; j++)
+				free(p[j]);
+			free(p);
+			return (NULL);
+		}
+	}
+	for (j = 0; j < width; j++)
+	{
+		p[i][j] = 0;
 	}
 	return (p);
 }
