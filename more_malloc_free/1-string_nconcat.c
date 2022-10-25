1ab2be539a70909e881102c76109b146d5c56b85
CllaudiaB <buraanna27@gmail.com>
2022-10-25 11:12:33 -0700

    task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,6 +1,24 @@
 #include "main.h"
 #include <stdlib.h>
 
+
+/**
+ *_len - foction calculate lenth of a string
+ *@s: parameter of the function
+ *
+ *Return: int
+ */
+
+
+int _len(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
 /**
  *string_nconcat -  function that concatenates two strings
  *@s1: string 1
@@ -19,10 +37,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	while (s1[len1] != '\0')
-		len1++;
-	while (s2[len2] != '\0')
-		len2++;
+	len1 = _len(s1);
+	len2 = _len(s2);
+
 	if (len2 > n)
 		len2 = n;
 	p = malloc(sizeof(char) * (len1 + len2 + 1));
