5e79a836041512a769927dc1937da1420be59ae9
CllaudiaB <buraanna27@gmail.com>
2022-10-31 02:44:36 -0700

    task 0

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ *struct dog - type structure
+ *@name: First member
+ *@age: Second member
+ *@owner: Third member
+ *Description: my struct dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H */
