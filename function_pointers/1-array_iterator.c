d7f11c52959994107ec86c002b8cf7e2d8a7440a
CllaudiaB <buraanna27@gmail.com>
2022-11-02 03:18:26 -0700

    task 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ *array_iterator
+ *@array: array to print
+ *@size_t: size of array
+ *@action: function pointer
+ *
+ *Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned long int i = 0;
+
+	if (array == NULL || size <= 0 || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
