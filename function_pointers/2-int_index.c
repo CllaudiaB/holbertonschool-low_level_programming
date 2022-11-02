29d8433de52fa18f3a24d6289339dc8846adb17f
CllaudiaB <buraanna27@gmail.com>
2022-11-02 07:20:39 -0700

    task 2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,13 +13,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (
+
 	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
 
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]) != 0)
-		    return (i);
+			return (i);
 	}
-		return (0);
+		return (-1);
 }
