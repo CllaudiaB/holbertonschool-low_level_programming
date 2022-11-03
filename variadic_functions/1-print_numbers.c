09ea07a5958ac2d6155365cbfedb1d0a6d0790d8
CllaudiaB <buraanna27@gmail.com>
2022-11-03 11:13:17 -0700

    task 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -28,7 +28,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			if (i < n - 1)
 				printf("%c ", *separator);
 		}
-		else if (separator == NULL || *separator == ' ')
+		else if (separator == NULL && *separator == ' ')
 			printf("%d", nb);
 
 	}
