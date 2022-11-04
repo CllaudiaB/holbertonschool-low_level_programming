4b995c0f4bf2743a753dd920a4ced6332f296fe9
CllaudiaB <buraanna27@gmail.com>
2022-11-04 11:42:28 -0700

    task 3

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,25 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
+
+/**
+ *struct _print - structure
+ *@op: option
+ *@f: fonction pointer
+ *
+ */
+
+typedef struct _print
+{
+	char *op;
+	void (*f)(va_list arg);
+}print_t;
+
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
 #endif /* _VARIADIC_FUNCTIONS_H */
