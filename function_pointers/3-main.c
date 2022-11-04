c1484d7899d95b264897220d39fa9ad552c55c49
CllaudiaB <buraanna27@gmail.com>
2022-11-04 09:28:04 -0700

    task 3

---
+++ function_pointers/3-main.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (int argc,char *argv[])
+{
+	int n1, n2, res = 0;
+	char *s;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+	s = argv[2];
+	if (s == NULL || get_op_func(s) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	res = get_op_func(s)(n1, n2);
+	printf("%d\n", res);
+
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,78 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *op_add - function addition
+ *@a: number1
+ *@b: number 2
+ *
+ *Return: sum o
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *op_sub - function substraction
+ *@a: number 1
+ *@b: number 2
+ *
+ *Return: difference
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ *op_mul - function multiplication
+ *@a: number 1
+ *@b: number 2
+ *
+ *Return: product
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ *op_div - function division
+ *@a: number 1
+ *@b: number 2
+ *
+ *Return: result of the division
+ */
+
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return(a / 2);
+}
+
+/**
+ *op_mod - function  modulo
+ *@a: number 1
+ *@b: number 2
+ *
+ *Return: remainder of the division
+ */
+
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
