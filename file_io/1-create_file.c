7e75ea10ba9222089f65840024267f4f902f9008
CllaudiaB <buraanna27@gmail.com>
2022-11-22 01:42:41 -0800

    task 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,8 +19,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	for (i = 0; text_content[i] != '\0'; i++)
 	{
-		write(fd, text_content, i);
+
 	}
+	if (text_content != NULL)
+		write (fd, text_content, i);
 	close(fd);
 	return (1);
 }
